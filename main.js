4f482c01bd1911af84e16c599068bb32db4b0bab
Amrryzen <alvinnoval001@gmail.com>
2026-02-13 16:01:05 +0700

    Styling Home, dan Landing Page

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const landing = document.querySelector(".landing");
-const home = document.getElementById("home");
 const btn = document.querySelector(".enter-btn");
+const homeNav = document.querySelector('nav a[href="#landing"]');
 
+// Masuk ke konten utama
 btn.addEventListener("click", function(e) {
   e.preventDefault();
-
   landing.classList.add("active");
-
+  
   setTimeout(() => {
     landing.style.display = "none";
-    home.scrollIntoView({ behavior: "smooth" });
-  }, 800);
+    // Scroll ke konten setelah landing
+    document.getElementById("header").scrollIntoView({ behavior: "smooth" });
+  }, 700);
+});
+
+// Balik ke Landing dari Navbar
+homeNav.addEventListener("click", function(e) {
+    e.preventDefault();
+    
+    // Munculkan kembali landing
+    landing.style.display = "flex";
+    
+    // Scroll ke paling atas dulu
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    
+    // Hilangkan blur/efek aktif
+    setTimeout(() => {
+        landing.classList.remove("active");
+    }, 10);
 });
